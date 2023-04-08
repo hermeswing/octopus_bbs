09d18166c256d2e3f40a96f46e142e85709ece4e
hermeswing <hermeswing@naver.com>
2023-04-08 11:11:04 +0900

    JPA를 이용한 게시판 개발

---
+++ src/main/java/octopus/OctopusBbsApplication.java
@@ -0,0 +1,13 @@
+package octopus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctopusBbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctopusBbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/octopus/OctopusBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package octopus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctopusBbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
