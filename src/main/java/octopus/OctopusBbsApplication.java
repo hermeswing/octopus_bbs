41a6c758bbee01cc6f37e0314b53dffdb8662dda
hermeswing <hermeswing@naver.com>
2023-04-13 00:25:55 +0900

    index.html 생성

--- src/main/java/octopus/OctopusBbsApplication.java
+++ src/main/java/octopus/OctopusBbsApplication.java
@@ -2,12 +2,14 @@ package octopus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class OctopusBbsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(OctopusBbsApplication.class, args);
-	}
-
+    
+    public static void main(String[] args) {
+        SpringApplication.run(OctopusBbsApplication.class, args);
+    }
+    
 }
