eb13a66fe4c0adb4166ae31775272046819913bd
hermeswing <hermeswing@naver.com>
2023-04-15 16:39:46 +0900

    확인

--- src/main/java/octopus/OctopusBbsApplication.java
+++ src/main/java/octopus/OctopusBbsApplication.java
@@ -2,7 +2,9 @@ package octopus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // @EntityListeners 를 사용할 수 있도록 함.
 @SpringBootApplication
 public class OctopusBbsApplication {
     
